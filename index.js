d6f78e040b08e4ef466aefaa06c8eda1e3b40154
ozcanovunc <ozcanovunc@outlook.com>
2018-02-14 22:46:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-if (global.Device.deviceOS === "iOS") {
-  module.exports = require('./extendedlabel-iOS');
-} else if (global.Device.deviceOS === "Android") {
-  module.exports = require('./extendedlabel-Android');
-}
+module.exports = require(`./extendedlabel-${global.Device.deviceOS}`);
